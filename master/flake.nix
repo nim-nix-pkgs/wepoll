2a047e81a95ddacd90625b2c5968abe9825692e6
hugosenari <hugosenari@gmail.com>
2022-03-12 02:06:10 -0300

    chore: re index wepoll

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Windows epoll wrapper.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-wepoll-master.flake = false;
+  inputs.src-wepoll-master.owner = "xflywind";
+  inputs.src-wepoll-master.ref   = "refs/heads/master";
+  inputs.src-wepoll-master.repo  = "wepoll";
+  inputs.src-wepoll-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-wepoll-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-wepoll-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
