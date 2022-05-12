e48921bdc8ec76e0723da049252bbc26dd5c9b0c
hugosenari <hugosenari@gmail.com>
2022-05-12 11:58:53 -0300

    chore: re index wepoll

--- flake.nix
+++ flake.nix
@@ -7,6 +7,22 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."wepoll-master".dir   = "master";
+  inputs."wepoll-master".owner = "nim-nix-pkgs";
+  inputs."wepoll-master".ref   = "master";
+  inputs."wepoll-master".repo  = "wepoll";
+  inputs."wepoll-master".type  = "github";
+  inputs."wepoll-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."wepoll-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."wepoll-v0_1_0".dir   = "v0_1_0";
+  inputs."wepoll-v0_1_0".owner = "nim-nix-pkgs";
+  inputs."wepoll-v0_1_0".ref   = "master";
+  inputs."wepoll-v0_1_0".repo  = "wepoll";
+  inputs."wepoll-v0_1_0".type  = "github";
+  inputs."wepoll-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."wepoll-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
